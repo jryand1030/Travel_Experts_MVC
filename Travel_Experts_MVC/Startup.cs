b3a22514717fa4177ae12d18fbfe32b45b123abe
jryand1030 <40072313+jryand1030@users.noreply.github.com>
2019-05-08 10:43:11 -0600

    Add project files.

---
+++ Travel_Experts_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Travel_Experts_MVC.Startup))]
+namespace Travel_Experts_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
